bc7dff440aeba3d88fb92f79accfda3bdcab265e
showrov4g <showrovg00@gmail.com>
2024-12-23 22:56:44 +0600

    Firebase enverioment variable Setup

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBwrn0NcwUCzOdttzpdm5SH-xthnLfkhgg",
-  authDomain: "assignment11-495e5.firebaseapp.com",
-  projectId: "assignment11-495e5",
-  storageBucket: "assignment11-495e5.firebasestorage.app",
-  messagingSenderId: "414287410860",
-  appId: "1:414287410860:web:75e47ddd689c57645f5109",
-  measurementId: "G-MWEYJW82EG"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId:import.meta.env.VITE_measurementId
 };
 
 // Initialize Firebase
