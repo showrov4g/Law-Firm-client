e300d8d038d3fc517944044b86b116af96c7c93e
showrov4g <showrovg00@gmail.com>
2024-12-25 21:50:53 +0600

    all services page design finished

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Services = () => {
   const data = useLoaderData();
+  const {serviceName, serviceArea, serviceImage, servicePrice, description, displayName,photoURL} = data;
   return (
     <div>
       <h2 className="text-center text-5xl font-bold">
@@ -10,21 +11,33 @@ const Services = () => {
       </h2>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 my-6">
         {data?.map((service) => (
-          <div className="card card-compact bg-base-100  shadow-xl">
-            <figure>
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Buy Now</button>
-              </div>
-            </div>
-          </div>
+               <div>
+               <div className="card bg-base-100 shadow-xl">
+                 <figure>
+                   <img src={service.serviceImage} alt="Shoes" />
+                 </figure>
+                 <div className="card-body">
+                   <h2 className="card-title">
+                     {service.serviceName}
+                     <div className="badge badge-secondary">
+                       Price:$ {service.servicePrice}
+                     </div>
+                   </h2>
+                   <p>{service.description.substring(0, 100)}</p>
+                   <div className="flex gap-5 justify-center items-center">
+                     <div className="avatar">
+                       <div className="ring-primary ring-offset-base-100 w-10 rounded-full ring ring-offset-2">
+                         <img src={service.photoURL} />
+                       </div>
+                     </div>
+                     <p className="font-semibold">Service Provider: <span className="badge badge-secondary">{service.displayName}</span></p>
+                   </div>
+                   <div className="card-actions justify-end">
+                     <Link to={`/servicesDetails/${service._id}`} className=" btn badge badge-outline">Views Details</Link>
+                   </div>
+                 </div>
+               </div>
+             </div>
         ))}
       </div>
     </div>
