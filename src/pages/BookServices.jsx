0e1a5815bb37a9cfc1e83d44361558b504c9f85a
showrov4g <showrovg00@gmail.com>
2024-12-25 13:29:54 +0600

    Booked services page design

--- src/pages/BookServices.jsx
+++ src/pages/BookServices.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../Context/AuthProvider';
 
 const BookServices = () => {
+    const {user} = useContext(AuthContext);
+    const [services, setServices] = useState([]);
+    useEffect(()=>{
+        fetch(`https://server-rho-liart-69.vercel.app/my-purchase?email=${user?.email}`)
+    },[])
     return (
         <div>
             
