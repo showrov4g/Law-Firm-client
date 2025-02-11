03038ebad245d93871b4f6b38aa4b4136754ec66
showrov4g <showrovg00@gmail.com>
2025-02-11 09:04:46 +0600

    Navbar color issue fixed

--- src/Compontents/NavBar.jsx
+++ src/Compontents/NavBar.jsx
@@ -7,109 +7,126 @@ const NavBar = () => {
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
-  useEffect(()=>{
-    localStorage.setItem("theme", theme)
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
     const localTheme = localStorage.getItem("theme");
-    document.querySelector("html").setAttribute("data-theme", localTheme)
-   },[theme])
+    document.querySelector("html").setAttribute("data-theme", localTheme);
+  }, [theme]);
   const themeController = (e) => {
-    if(e.target.checked){
-      setTheme("dark")
-    }else(
-      setTheme("light")
-    )
+    if (e.target.checked) {
+      setTheme("dark");
+    } else setTheme("light");
   };
   const links = (
     <>
-       <li><NavLink to={"/"}>Home</NavLink></li>
-       <li><NavLink to={"/services"}>services</NavLink></li>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/services"}>services</NavLink>
+      </li>
       <li>
         <details>
           <summary>Dashboard</summary>
           <ul className="p-2 w-80 z-50">
-            <li><NavLink to={"/addAService"}>Add Services</NavLink></li>
-            <li><NavLink to={'/manageService'}>Manage Services</NavLink></li>
-            <li><NavLink to={'/bookServices'}>Booked-Services</NavLink></li>
-            <li><NavLink to={'/serviceToDo'}>Service to do</NavLink></li>
+            <li>
+              <NavLink to={"/addAService"}>Add Services</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/manageService"}>Manage Services</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/bookServices"}>Booked-Services</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/serviceToDo"}>Service to do</NavLink>
+            </li>
           </ul>
         </details>
       </li>
     </>
   );
   return (
-    <div className="navbar bg-gray-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 p-2 shadow"
-          >
-            {links}
-          </ul>
-        </div>
-        <Link to="/" className="btn btn-ghost text-xl">LAW</Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{links}</ul>
-      </div>
-      <input
-        onChange={themeController}
-        type="checkbox"
-        checked={theme === "light" ? false : true}
-        value="synthwave"
-        className="toggle theme-controller"
-      />
-      <div className="navbar-end">
-        {user && user?.email ? (
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img alt="Tailwind CSS Navbar component" src={user?.photoURL} />
-              </div>
+    <div className="bg-gray-100">
+      <div className="navbar ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 p-2 shadow"
             >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a onClick={logOut}>Logout</a>
-              </li>
+              {links}
             </ul>
           </div>
-        ) : (
-          <Link className="btn" to={"/login"}>
-            Login
+          <Link to="/" className="btn btn-ghost text-xl">
+            LAW
           </Link>
-        )}
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <input
+          onChange={themeController}
+          type="checkbox"
+          checked={theme === "light" ? false : true}
+          value="synthwave"
+          className="toggle theme-controller"
+        />
+        <div className="navbar-end">
+          {user && user?.email ? (
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src={user?.photoURL}
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a onClick={logOut}>Logout</a>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <Link className="btn" to={"/login"}>
+              Login
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
